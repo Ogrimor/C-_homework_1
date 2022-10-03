76dd026535b17e83eb4679e6c9eaf6d99aeffbe4
Ogrimor <ogrimors37@gmail.com>
2022-10-03 10:20:01 +0500

    Number difference

---
+++ number_difference/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число: ");
+string s1 = Console.ReadLine();
+Console.WriteLine("Введите второе число: ");
+string s2 = Console.ReadLine();
+int firstNumber = int.Parse(s1);
+int secondNumber = int.Parse(s2);
+if ( firstNumber > secondNumber)
+{
+    Console.WriteLine("Первое число, больше второго");
+}
+else if (firstNumber < secondNumber)
+{
+    Console.WriteLine("Второе число, больше первого");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}

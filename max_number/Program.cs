9fcf0c5a8d5955c65ff82152495eb95591fcec0c
Ogrimor <ogrimors37@gmail.com>
2022-10-03 10:30:54 +0500

    Max number

---
+++ max_number/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число: ");
+string s1 = Console.ReadLine();
+Console.WriteLine("Введите второе число: ");
+string s2 = Console.ReadLine();
+Console.WriteLine("Введите третье число: ");
+string s3 = Console.ReadLine();
+int firstNumber = int.Parse(s1);
+int secondNumber = int.Parse(s2);
+int lastNumber = int.Parse(s3);
+int max = firstNumber;
+if (max < secondNumber)
+{
+    max = secondNumber;
+}
+else if(max < lastNumber)
+{
+    max = lastNumber;
+}
+Console.WriteLine("Вычисляем...");
+Console.WriteLine("Наибольшее число: " + max);

40101497b596b0bd7d38b8f7654115b42b0e61b3
Ogrimor <ogrimors37@gmail.com>
2022-10-03 11:05:11 +0500

    Numbers even number

---
+++ number_of_even_number/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+string s1 = Console.ReadLine();
+int number = int.Parse(s1);
+int i;
+Console.WriteLine("Чётные числа, от 0 до " + number);
+for (i = 0; i <= number; i++)
+{
+    if (i%2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}

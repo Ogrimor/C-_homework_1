c91bf3ed3a7cac15e792a015f78e078cda90c934
Ogrimor <ogrimors37@gmail.com>
2022-10-03 10:47:44 +0500

    Even number

---
+++ even_number/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+string s1 = Console.ReadLine();
+int number = int.Parse(s1);
+if (number%2 == 0)
+{
+    Console.WriteLine("Введённое Вами число, является чётным");
+}
+else
+{
+    Console.WriteLine("Введённое Вами число не является чётным");
+}
